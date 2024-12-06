f5e0b4ca9046050783925b9d710b3687bfddb1f5
RobeenAziz <robeen.aziz115@gmail.com>
2024-12-05 19:53:47 -0800

    Update Matrices.cpp

--- Particles/Matrices.cpp
+++ Particles/Matrices.cpp
@@ -81,7 +81,6 @@ namespace Matrices
         }
         return out;
     }
-}
 
 ScalingMatrix::ScalingMatrix(double scale)
 Matrix(2, 2);
@@ -89,3 +88,6 @@ Matrix(2, 2);
     a[0][0] = scale;
     a[1][1] = scale;
 }
+}
+
+
