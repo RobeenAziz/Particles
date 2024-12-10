facae80e12dcf8277567fe596fa1d0fcc46f8d67
RobeenAziz <robeen.aziz115@gmail.com>
2024-12-09 20:21:36 -0800

    Update Particle.cpp

--- Particles/Particle.cpp
+++ Particles/Particle.cpp
@@ -44,7 +44,7 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 {
 	VertexArray lines(TriangleFan, m_numPoints + 1);
 	Vector2f center;
-	center.mapPixelToCoords(m_centerCoordinate, m_cartesianPlane);
+	target.mapPixelToCoords(m_centerCoordinate, m_cartesianPlane);
 	lines[0].position = center;
 	lines[0].color = m_color1;
 	for (int j = 1; j <= m_numPoints; ++j)
@@ -52,7 +52,7 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 		lines[j].position = ; 
 		lines[j].color = m_color2;
 	}
-	center.draw(lines)
+	target.draw(lines)
 }
 
 void update(float dt)
