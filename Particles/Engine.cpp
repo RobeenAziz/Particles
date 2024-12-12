2d622f07660c5d63cb75faad9f6ffd9394af0567
RobeenAziz <robeen.aziz115@gmail.com>
2024-12-11 19:09:21 -0800

    Update Engine.cpp

--- Particles/Engine.cpp
+++ Particles/Engine.cpp
@@ -1,1 +1,26 @@
 #include "Engine.h"
+
+Engine::Engine()
+{
+	m_Window.create(VideoMode::getDesktopMode(), "Particle System");
+}
+
+void Engine::run()
+{
+	Clock clock;
+	cout << "Starting Particle unit tests..." << endl;
+	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+	p.unitTests();
+	cout << "Unit tests complete.  Starting engine..." << endl;
+	while (m_Window.isOpen())
+	{
+		clock.restart();
+		Time curtime;
+		float seconds;
+		curtime = clock.getElapsedTime();
+		seconds = curtime.asSeconds();
+		input();
+		update(seconds);
+		draw();
+	}
+}
